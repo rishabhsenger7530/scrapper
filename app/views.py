46c3565d9b619425f5e7a24a6acd84ae3e3bab54
meena6 <meena@nantucketrentals.com>
2022-07-12 23:52:58 +0530

    change in views

--- app/views.py
+++ app/views.py
@@ -83,7 +83,7 @@ def home(request):
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--start-maximized')
         chrome_options.add_argument('--disable-setuid-sandbox')
-
+        chrome_options.add_argument('--enable-features=NetworkService,NetworkServiceInProcess')
         driver = webdriver.Chrome(
             executable_path=DRIVER_PATH, chrome_options=chrome_options)
         driver.maximize_window()
