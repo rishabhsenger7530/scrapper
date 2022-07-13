0a7361a23ea6cddf0c43f39ef5db6a1ff8d0337c
meena6 <meena@nantucketrentals.com>
2022-07-13 19:50:49 +0530

    5

--- app/views.py
+++ app/views.py
@@ -64,13 +64,15 @@ def home(request):
         chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
         # chrome_options.add_argument('--disable-gpu')
         chrome_options.add_argument('--headless')
+        chrome_options.add_argument("--disable-dev-shm-usage")
+
         chrome_options.add_argument('--window-size=1920,1080')
         chrome_options.add_argument('--no-sandbox')
-        chrome_options.add_argument('--start-maximized')
+        # chrome_options.add_argument('--start-maximized')
         # chrome_options.add_argument('--disable-setuid-sandbox')
-        chrome_options.add_argument("--remote-debugging-port=9222")
+        # chrome_options.add_argument("--remote-debugging-port=9222")
         from webdriver_manager.chrome import ChromeDriverManager
-        driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"),
+        driver = webdriver.Chrome(executable_path=DRIVER_PATH,
                                   chrome_options=chrome_options)
         # driver = webdriver.Chrome(
         #     ChromeDriverManager().install(), chrome_options=chrome_options)
